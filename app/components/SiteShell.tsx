cdfec8fb6ecd574bcd685316105155aa951a0c43
Delibes <delibes.ats@gmail.com>
2026-07-29 02:29:36 -0600

    correcion flechas

--- app/components/SiteShell.tsx
+++ app/components/SiteShell.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import ArrowUpRight from "./ArrowUpRight";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
